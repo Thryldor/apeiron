7d7353ef63483e573de83f80d42428ec9d6bc3d5
Arzhel <arzhel36@gmail.com>
2020-05-07 21:34:04 +0200

    monde mort

---
+++ Assets/checkPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkPoint : MonoBehaviour
+{
+    public GameObject[] playerSpawn;
+    private GameObject player;
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Update()
+    {
+        for(int i = 1; i < playerSpawn.Length-1; i++)
+        {
+            if(player.transform.position.x < playerSpawn[i].transform.position.x && player.transform.position.x > playerSpawn[i - 1].transform.position.x)
+            {
+                transform.position = playerSpawn[i].transform.position;
+            }
+        }
+    }
+}
