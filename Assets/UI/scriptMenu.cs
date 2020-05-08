2d8fd31502774b3afd966b7656ff88976d601cc5
tnt175 <julienbob_17@hotmail.com>
2020-05-08 13:15:37 -0400

    Ajout des sons de menu

--- Assets/UI/scriptMenu.cs
+++ Assets/UI/scriptMenu.cs
@@ -5,9 +5,23 @@ using System.Collections;
 
 public class scriptMenu : MonoBehaviour
 {
+    public AudioSource musicMenu;
+    public AudioSource buttonSound;
+    public GameObject buttonObject;
+
+    void Awake(){
+        DontDestroyOnLoad(buttonObject);
+    }
+    void Start()
+    {
+        musicMenu.volume = PlayerPrefs.GetFloat("soundVol",1);
+        buttonSound.volume = PlayerPrefs.GetFloat("soundVol",1);
+    }
     public void GoToLevel()
     {
         SceneManager.LoadScene("Game");
+        buttonSound.Play();
+        musicMenu.Stop();
     }
     public void GoToOptions()
     {
